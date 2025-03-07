f95fe0a1a9753036c4cb7ddbdcd5570548fff607
Edmond Huang <huane871@newschool.edu>
2025-03-07 11:40:49 -0500

    Update 2

--- Assets/Scripts/Base Movement.cs
+++ Assets/Scripts/Base Movement.cs
@@ -7,11 +7,19 @@ public class BaseMovement : MonoBehaviour
     public float moveSpeed;
     public float groundDrag;
 
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask Ground;
     //private bool grounded;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
 
     public Transform orientation;
 
@@ -34,23 +42,38 @@ public class BaseMovement : MonoBehaviour
 
         if (Grounded())
         {
+            // adds drag or friction to movement
             rb.linearDamping = groundDrag;
-
-
         }
         else
+        {
             rb.linearDamping = 0f;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            Walk();
-        else
-            Sprint();
+        }
     }
 
     private void GetInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+
+        if (Input.GetKey(jumpKey) && readyToJump && Grounded())
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+
+
+        if (Input.GetKeyDown(sprintKey))
+            Walk();
+        else
+            Sprint();
+
+
     }
 
     private void Walk()
@@ -65,6 +88,19 @@ public class BaseMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * moveSpeed * 125f * Time.deltaTime, ForceMode.Force);
+    }
+
+    private void Jump()
+    {
+        //reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(this.transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 
     private bool Grounded()
